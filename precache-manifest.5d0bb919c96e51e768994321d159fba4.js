e5f349c62a52d2aae541f0baccb55bf4a37f6821
krystiano13 <krystiano13@interklasa.pl>
2020-05-02 21:39:44 +0200

    first commit

---
+++ precache-manifest.5d0bb919c96e51e768994321d159fba4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b69e4e2b2209c929f1dfaaa74fe32c1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e6ac424149b4de3b6ba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "47eee8f65cc6b2ea3fe0",
+    "url": "/static/js/2.f21b324b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f21b324b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e6ac424149b4de3b6ba",
+    "url": "/static/js/main.53dc579e.chunk.js"
+  },
+  {
+    "revision": "0cd47e8278b2724b367f",
+    "url": "/static/js/runtime-main.27aae9d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
